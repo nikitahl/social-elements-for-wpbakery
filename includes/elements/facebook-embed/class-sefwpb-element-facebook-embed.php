020b2391e24018f3e83c50e7d7204eec09cebb8b
Nikita <nikita.h@visualcomposer.com>
2025-08-27 11:56:57 +0300

    Add Facebook Embed

---
+++ includes/elements/facebook-embed/class-sefwpb-element-facebook-embed.php
@@ -0,0 +1,59 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'WPBakeryShortCode' ) ) {
+
+	/**
+	 * WPBakery Element: Facebook Embed
+	 *
+	 * Embeds Facebook post.
+	 *
+	 * @since 1.0.0
+	 */
+	class WPBakeryShortCode_Sefwpb_Facebook_Embed extends WPBakeryShortCode {
+		/**
+		 * Shortcode output.
+		 *
+		 * @param array  $atts    Shortcode attributes.
+		 * @param string $content Shortcode content.
+		 * @return string
+		 */
+		public function content( $atts, $content = '' ) {
+			$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+			$el_class = isset( $atts['el_class'] ) ? $atts['el_class'] : '';
+			$css      = isset( $atts['css'] ) ? $atts['css'] : '';
+			$css_animation = isset( $atts['css_animation'] ) ? $atts['css_animation'] : '';
+
+			$style = '';
+			$css_classes = [ 'sefwpb-element', 'sefwpb-pinterest-embed', $el_class, $this->getCSSAnimation( $css_animation ) ];
+			if ( ! empty( $el_class ) ) {
+				$css_classes[] = $el_class;
+			}
+			if ( ! empty( $css ) ) {
+				$css_classes[] = vc_shortcode_custom_css_class( $css );
+			}
+			if ( isset( $atts['align'] ) ) {
+				$style .= 'text-align: ' . $atts['align'] . ';';
+			}
+			$css_class = implode( ' ', array_filter( $css_classes ) );
+			$el_id = ! empty( $atts['el_id'] ) ? 'id="' . esc_attr( $atts['el_id'] ) . '"' : '';
+			$output = '<div ' . $el_id . ' class="' . esc_attr( $css_class ) . '">';
+			if ( ! empty( $atts['title'] ) ) {
+				$output .= '<h3 class="sefwpb-facebook-embed-title">' . esc_html( $atts['title'] ) . '</h3>';
+			}
+			$output .= '<div class="sefwpb-facebook-embed-container" style="' . esc_attr( $style ) . '">';
+			$output .= '<div class="fb-post" data-href="' . esc_url( $atts['url'] ) . '" data-width="' . esc_attr( $atts['width'] ) . '"></div>';
+			$output .= '<div id="fb-root"></div>';
+			$output .= '<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="SEFWPB"></script>';
+			$output .= '<script>';
+			$output .= 'if (typeof FB !== "undefined" && FB.XFBML && FB.XFBML.parse) {';
+			$output .= 'FB.XFBML.parse();';
+			$output .= '}';
+			$output .= '</script>';
+			$output .= '</div>'; // .sefwpb-facebook-embed-container
+			$output .= '</div>'; // .sefwpb-element
+			return $output;
+		}
+	}
+}
---
+++ includes/elements/facebook-embed/index.php
@@ -0,0 +1,72 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+add_action( 'vc_before_init', function () {
+	require_once __DIR__ . '/class-sefwpb-element-facebook-embed.php';
+
+	vc_map(
+		[
+			'name'              => esc_html__( 'Facebook Embed', SEFWPB_TD ),
+			'base'              => 'sefwpb_facebook_embed',
+			'description'       => esc_html__( 'Embed a single Facebook post easily.', SEFWPB_TD ),
+			'category'          => esc_html__( 'Social', SEFWPB_TD ),
+			'icon'              => 'sefwpb-icon-facebook-embed',
+			'params'            => [
+				[
+					'type' => 'textfield',
+					'heading' => esc_html__('Title', SEFWPB_TD),
+					'param_name' => 'title',
+					'description' => esc_html__('Optional. Enter title to display above embed.', SEFWPB_TD),
+				],
+				[
+					'type' => 'textfield',
+					'heading' => esc_html__('Facebook Post URL', SEFWPB_TD),
+					'param_name' => 'url',
+					'description' => esc_html__('Enter the URL of the Facebook post you want to embed. Example: https://www.facebook.com/100064860875397/posts/10154009990506729/', SEFWPB_TD),
+					'value' => 'https://www.facebook.com/natgeo/posts/pfbid0YKtpC5jfd6EksBDfCFBwAAdhUUWJkY4QUjHWSpyPEesiACiSTFptW2tdFvmqCN3rl',
+				],
+				[
+					'type' => 'dropdown',
+					'heading' => esc_html__( 'Alignment', SEFWPB_TD ),
+					'param_name' => 'align',
+					'group' => esc_html__( 'Styles', SEFWPB_TD ),
+					'value' => [
+						esc_html__( 'Left', SEFWPB_TD ) => 'left',
+						esc_html__( 'Center', SEFWPB_TD ) => 'center',
+						esc_html__( 'Right', SEFWPB_TD ) => 'right',
+					],
+					'std' => 'left',
+				],
+				[
+					'type'        => 'textfield',
+					'heading'     => esc_html__( 'Width', SEFWPB_TD ),
+					'param_name'  => 'width',
+					'group' => esc_html__( 'Styles', SEFWPB_TD ),
+					'description' => esc_html__( 'Optional. Enter width of the embedded post in pixels. Default is 500px.', SEFWPB_TD ),
+					'value'       => '500',
+				],
+				vc_map_add_css_animation(),
+				[
+					'type'        => 'el_id',
+					'heading'     => esc_html__( 'Element ID', SEFWPB_TD ),
+					'param_name'  => 'el_id',
+					// translators: %1$s: link to w3c specification, %2$s: closing anchor tag.
+					'description' => sprintf( esc_html__( 'Enter element ID (Note: make sure it is unique and valid according to %1$sw3c specification%2$s).', SEFWPB_TD ), '<a href="https://www.w3schools.com/tags/att_global_id.asp" target="_blank">', '</a>' ),
+				],
+				[
+					'type'        => 'textfield',
+					'heading'     => esc_html__( 'Extra class name', SEFWPB_TD ),
+					'param_name'  => 'el_class',
+					'description' => esc_html__( 'If you wish to style particular content element differently, then use this field to add a class name and then refer to it in your css file.', SEFWPB_TD ),
+				],
+				[
+					'type'       => 'css_editor',
+					'heading'    => esc_html__( 'CSS', SEFWPB_TD ),
+					'param_name' => 'css',
+					'group'      => esc_html__( 'Design Options', SEFWPB_TD ),
+				],
+			]
+		]
+	);
+} );
